ea93f86ea83a7e676805acb10939e6f553b16c55
raemercabrera29 <raemercabrera.school@gmail.com>
2026-07-14 10:12:01 +0800

    First

---
+++ cabrerainf239/src/App.js
@@ -0,0 +1,117 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>John Raemer Cabrera</h1>
+        <p>BS Information Technology Student</p>
+        <p>Aspiring Software Developer | Web Developer | Tech Enthusiast</p>
+        <img src="https://github.com/raemercabrera29/prj2/blob/main/MONE.png?raw=true" alt="Description of the image"></img>
+      </header>
+
+      <section>
+        <h2>About Me</h2>
+        <p>
+          Hello! I am <strong>John Raemer Cabrera</strong>,  
+          a motivated and detail-oriented Information technology student with a strong
+          passion for problem-solving, continuous learning, and innovation. Experienced
+          in leadership roles and collaborative projects throughout primary, secodary, and 
+          tertiary education education. Commited to applying technical and interpersonal skills to
+          develop practical solutions and contribute positively to the academic and professional
+          community.
+        </p>
+      </section>  
+
+      <section>
+        <h2>Education</h2>
+        <p>
+          <strong>Bachelor of Science in Information Technology (BSIT)</strong>
+          <br />
+          Currently pursuing studies in web development, database management,
+          software engineering, networking, and systems analysis.
+        </p>
+      </section>
+
+      <section>
+        <h2>Technical Skills</h2>
+        <ul>
+          <li>HTML</li>
+          <li>JavaScript</li>
+          <li>React.js</li>
+          <li>Python</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <strong>Distinct.MNL Website </strong> – A Modern Website to promote and sell Aparells and Accesory. 
+          </li>
+          <li>
+            <strong>Lumispire</strong> – An AI-Enabled Service Management System for Hospitality, Training, and Manpower Services of the LTC Group.
+          </li>
+          <li>
+            <strong>SAFE MAP</strong> – A System that can help other people to contact emergenct hotlines easily.
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Interests</h2>
+        <ul>
+          <li>Web Development</li>
+          <li>Software Engineering</li>
+          <li>Social Media Marketing</li>
+          <li>Technology Innovation</li>
+          <li>Programming and Problem Solving</li>
+          <li>Gaming and Digital Entertainment</li>
+          <li>Learning New Technologies</li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Achievements</h2>
+        <ul>
+          <li>Completed Integrative Programming Course</li>
+          <li>Successfully Built Web-Based Academic Projects</li>
+          <li>Continuously Expanding Programming Knowledge</li>
+          <li>Second Honor Dean's Lister - First Year (1st Term)</li>
+          <li>First Honor Dean's Lister - First Year (2nd Term)</li>
+          <li>Second Honor Dean's Lister - First Year (3rd Term)</li>
+          <li>First Honor Dean's Lister - Second Year (1st Term)</li>
+          <li>First Honor Dean's Lister - Second Year (2nd Term)</li>
+          <li>First Honor Dean's Lister - Second Year (3rd Term)</li>
+          <li>First Honor Dean's Lister - Third Year (2nd Term)</li>
+          <li>First Honor Dean's Lister - Third Year (3rd Term)</li>
+        </ul>
+      </section>
+
+
+      <section>
+        <h2>Career Goals</h2>
+        <p>
+          My goal is to become a professional Software Developer and contribute
+          to innovative projects that solve real-world problems. I aim to
+          improve my expertise in web development, software engineering, and
+          emerging technologies while continuously learning and growing in the
+          field of Information Technology.
+        </p>
+      </section>
+
+      <section className="contact">
+        <h2>Contact Information</h2>
+        <p>📧 Email: raemercabrera.school@gmail.com</p>
+        <p>💻 GitHub: github.com/raemer29</p>
+        <p>📱 Facebook: John Raemer Cabrera</p>
+      </section>
+
+      <footer>
+        <p>© 2026 John Raemer Cabrera. All Rights Reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
